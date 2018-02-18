64d3f3cca2249883e22c91fdf40547cd6c394393
haydonryan <haydon.ryan@gmail.com>
2018-02-18 14:55:32 -0600

    [#155299979] updating build task to install glide and fix go directory

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -8,6 +8,12 @@ export PATH=$GOPATH/bin:$PATH
 OUTPUT_DIR=$PWD/compiled-output
 SOURCE_DIR=$PWD/source-code    
 
+
+# Because this image doesn't have glide in it...
+go get github.com/Masterminds/glide
+
+
+
 # These need to be taken out into variables
 APP_NAME="tile-configurator"
 #GITHUB_URL="github.com/haydonryan/tile-configurator"      #### I don't like that this is hardcoded.... 
@@ -18,7 +24,7 @@ APP_NAME="tile-configurator"
 cd ${SOURCE_DIR} 
 
 # Copy the source code to the gopath since go will expect it there.
-WORKING_DIR=$GOPATH/src/${REPO_ADDRESS}              
+WORKING_DIR=$GOPATH/src/$REPO_ADDRESS
 mkdir -p ${WORKING_DIR}
 cp -R ${SOURCE_DIR}/* ${WORKING_DIR}/.
 cd ${WORKING_DIR}
