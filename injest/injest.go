05108773aac7e71be84cdef98b8fe0f19b2f274c
haydonryan <haydon.ryan@gmail.com>
2018-02-18 20:07:06 -0600

    [delivers #155238969] externalised property help, cleanup and error messaging

--- injest/injest.go
+++ injest/injest.go
@@ -1,46 +1,79 @@
 package injest
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"sort"
+
+	"github.com/Navops/yaml"
 )
 
 /// Injest required for go-flags
 ///-----------
 type Injest struct {
+	InputFile string `short:"i" long:"injest" description:"Filename to be injested" required:"true"`
 }
 
 /// go-flags callhack entry point
 func (c *Injest) Execute([]string) error {
-	fmt.Println("in execute")
+
+	base, _ := readJSON(c.InputFile)
+	ProcessInjest(base)
+
 	return nil
 }
 
-var help = map[string]string{
+func readJSON(filename string) (map[string]interface{}, error) {
+	// Open the properties file
+	base, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return nil, fmt.Errorf("Could not open File")
+	}
 
-	".properties.syslog":              "Syslog Forwarding\n# -----------------\n# Choose either enabled or disbled",
-	".properties.syslog.enabled.port": "The port for syslog",
-	/*".properties.backups":             "Backups\n# -----------------\n# (enabled | disbled)",
-
-	".properties.backups.azure.base_url": "Azure URL for XXX",
-	//".properties.backups.azure.container":       "<nil>",
-	//".properties.backups.azure.container_path":  "<nil>",
-	/*	".properties.backups.azure.storage_account": "Storage Account Name in XX Format",
-
-		".properties.backups.enable.access_key_id": "<nil>",
-		".properties.backups.enable.bucket_name":   "<nil>",
-		".properties.backups.enable.bucket_path":   "<nil>",
-		".properties.backups.enable.endpoint_url":  "<nil>",
-		".properties.backups.enable.region":        "<nil>",
-
-		".properties.backups.gcs.bucket_name": "<nil>",
-	".properties.backups.gcs.project_id": "Choose the GCS Project ID to back up your MySQL to:",*/
+	// Read file into map of interfaces
+	m := make(map[string]interface{})
+	err = json.Unmarshal([]byte(base), &m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+		return nil, fmt.Errorf("Could not unmartshall File")
+	}
+	return m, nil
 }
+
+func readYaml(filename string) (map[string]interface{}, error) {
+	// Open the properties file
+	base, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return nil, fmt.Errorf("Could not open File")
+	}
+
+	// Read file into map of interfaces
+	m := make(map[string]interface{})
+	err = yaml.Unmarshal([]byte(base), &m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+		return nil, fmt.Errorf("Could not unmartshall File")
+	}
+	return m, nil
+}
+
+var help = map[string]string{}
 
 func ProcessInjest(m interface{}) {
 	var sl map[string]interface{}
 	sl, correct := m.(map[string]interface{})
 
+	// Open the help file
+	help, err := readYaml("help.yml")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(help)
 	// unwrap properties
 	if correct {
 
@@ -60,19 +93,26 @@ func ProcessInjest(m interface{}) {
 
 			switch v.(map[string]interface{})["type"] {
 			case "simple_credentials":
-				fmt.Println(k)
+				//fmt.Println(k)
+				fmt.Println("< Simple_credentials not supported >")
 				delete(sl["properties"].(map[string]interface{}), k)
 
 				//currently these types are not supported - delete the keys
 			case "salted_credentials":
+				fmt.Println("< salted_credentials not supported >")
 				delete(sl["properties"].(map[string]interface{}), k)
 			case "secret":
+				fmt.Println("< secret not supported >")
 				delete(sl["properties"].(map[string]interface{}), k)
 			case "ip_ranges":
+				fmt.Println("< ip_ranges not supported >")
 				delete(sl["properties"].(map[string]interface{}), k)
 			case "network_address":
+				fmt.Println("< network_address not supported >")
 				delete(sl["properties"].(map[string]interface{}), k)
-
+				//case "dropdown_select":
+				//fmt.Println("< dropdown_select not nsupported >")
+				delete(sl["properties"].(map[string]interface{}), k)
 			case "text":
 				fallthrough
 			case "email": //email are just text fields but have regex checking
@@ -100,6 +140,7 @@ func ProcessInjest(m interface{}) {
 				delete(v.(map[string]interface{}), "type")
 				fmt.Printf("%v, %v\n", k, v)
 				fmt.Println()
+
 			default:
 				propertyType := v.(map[string]interface{})["type"]
 				fmt.Printf("Default: %v\n", propertyType)
