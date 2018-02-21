deb985525189e0974c8d9dfe976a3462f50ce883
haydonryan <haydon.ryan@gmail.com>
2018-02-21 15:52:19 -0600

    adding readjson and readyaml

--- tileproperties/tileproperties.go
+++ tileproperties/tileproperties.go
@@ -1,5 +1,14 @@
 package tileproperties
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/Navops/yaml"
+)
+
 type TileProperties struct {
 	Properties map[string]interface{}
 }
@@ -8,4 +17,42 @@ type TileProperties struct {
 func NewTileProperties() *TileProperties {
 
 	return &TileProperties{}
+}
+
+func (t *TileProperties) ReadJSON(filename string) (map[string]interface{}, error) {
+	// Open the properties file
+	base, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return nil, fmt.Errorf("Could not open File")
+	}
+
+	// Read file into map of interfaces
+	m := make(map[string]interface{})
+	err = json.Unmarshal([]byte(base), &m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+		return nil, fmt.Errorf("Could not unmartshall File")
+	}
+	t.Properties = m
+	return m, nil
+}
+
+func (t *TileProperties) ReadYAML(filename string) (map[string]interface{}, error) {
+	// Open the properties file
+	base, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return nil, fmt.Errorf("Could not open File")
+	}
+
+	// Read file into map of interfaces
+	m := make(map[string]interface{})
+	err = yaml.Unmarshal([]byte(base), &m)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+		return nil, fmt.Errorf("Could not unmartshall File")
+	}
+	t.Properties = m
+	return m, nil
 }
--- tileproperties/tileproperties_test.go
+++ tileproperties/tileproperties_test.go
@@ -14,6 +14,22 @@ var _ = Describe("Properties", func() {
 		m := NewTileProperties()
 		Expect(m).ShouldNot(BeNil())
 	})
+	Context("Input", func() {
+		It("Can  read a YAML file", func() {
+			m := NewTileProperties()
+			m.ReadYAML("fixtures/types.yml")
+			Expect(m).ShouldNot(BeNil())
+			Expect(m.Properties[".properties.optional_protections.enable.canary_poll_frequency"]).Should(Equal(69))
+
+		})
+		It("Can  read a JSON file", func() {
+			m := NewTileProperties()
+			m.ReadJSON("fixtures/types.json")
+			//Expect(m).Should(ContainElement(".properties.backup_options"))
+			Expect(m.Properties["properties"]).ShouldNot(BeNil())
+		})
+	})
+
 })
 
 func TestSuite(t *testing.T) {
